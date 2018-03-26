905f1d6b83d9e129afa2d7103690fefa67ce33b2
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-03-26 11:23:13 +0700

    Add {fruit} to views by ejs template

--- session.js
+++ session.js
@@ -1,27 +1,17 @@
 var express = require('express')
 var app = express()
-var session = require('express-session')
 
-app.use(
-  session({
-    secret: 'keyboard cat',
-    cookie: { maxAge: 60000 },
-    resave: true,
-    saveUninitialized: true
-  })
-)
-
-app.use(function(req, res, next) {
-  var sess = req.session
-
-  if (sess.views) sess.views++
-  else sess.views = 1
-  console.log(sess.views)
-  next()
-})
+app.set('views', './views')
+app.set('view engine', 'ejs')
 
 app.get('/', function(req, res) {
-  res.send('Session: ' + req.session.views)
+  res.send('ok')
+})
+
+app.get('/fruit', function(req, res) {
+  res.render('fruit', {
+    fruits: ['banana', 'apple']
+  })
 })
 
 app.listen(8000)
